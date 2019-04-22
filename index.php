5f874fd02426d124454fe7dc74462d76fda2ff1a
WoodyWolf <oyasumi.pung.pung228@gmail.com>
2019-04-22 23:40:34 +0300

    Testing of serializators Added yaml_storage.yaml

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__.'/src/JSONStorage.php';
+require_once DIR.'/vendor/autoload.php';
+require_once DIR.'/src/JSONStorage.php';
 
 use App\JSONStorage;
 
-$json = new JSONStorage();
+//testing of JSON and YAML serializators
 
+$json = new JSONStorage();
+$yaml = new \App\YAMLStorage();
 $json->set('book','nad_propastiu');
 $json->set('dog','corgie');
 if($json->has('book')){
@@ -14,4 +16,12 @@ if($json->has('book')){
 }
 $data = $json->get('dog');
 
-var_dump($data);
+$yaml->set('book','nad_propastiu');
+$yaml->set('dog','corgie');
+if($yaml->has('book')){
+    $yaml->remove('book');
+}
+
+$data1 = $yaml->get('dog');
+
+var_dump($data1 );
