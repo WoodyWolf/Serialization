1db6babaddcfc98b364a89e23b697383f1dc183c
WoodyWolf <49565833+WoodyWolf@users.noreply.github.com>
2019-04-22 23:08:29 +0300

    Update YAMLStorage.php

--- src/YAMLStorage.php
+++ src/YAMLStorage.php
@@ -7,6 +7,7 @@
  */
 
 namespace App;
+use Symfony\Component\Yaml\Yaml;
 
 
 
@@ -17,8 +18,8 @@ class YAMLStorage extends AbstractController
     {
         $arr = $this->getData();
         $arr[$key] = $value;
-        $dataJson = Yaml::dump($arr);
-        file_put_contents('yaml_storage.yaml', $dataJson);
+        $dataYaml = Yaml::dump($arr);
+        file_put_contents('yaml_storage.yaml', $dataYaml);
     }
 
     public function remove($key)
@@ -26,7 +27,7 @@ class YAMLStorage extends AbstractController
         $arr = $this->getData();
         if(isset($arr[$key])){
             unset($arr[$key]);
-            file_put_contents(yaml_storage.yaml,Yaml::dump($arr));
+            file_put_contents('yaml_storage.yaml',Yaml::dump($arr));
             return true;
         }
         else
